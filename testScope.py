89dbdc848b0bc00a487cd9dbb56d87a9d67a344d
Lukas van den Heuvel <2187cita@gmail.com>
2025-12-16 15:41:57 +0100

    update test file

--- testScope.py
+++ testScope.py
@@ -10,10 +10,17 @@ scope=fibsem()
 if __name__ == "__main__":
 
     img = scope.take_image_IB()
-    out = {
-        "img": img
-    }
+    
 
+    directory = ""
+    filename = ""
+    pattern = scope.pattern_parser(directory,filename)
+
+
+    out = {
+        "img": img,
+        "pattern": pattern
+    }
     with open('TEST.pickle', 'wb') as handle:
         pickle.dump(out, handle, protocol=pickle.HIGHEST_PROTOCOL)
     print("Saved TEST.pickle")
